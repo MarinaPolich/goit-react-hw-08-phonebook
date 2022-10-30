507a689e69956a93df4e8c8efe13e112567e269f
MarinaPolich <marina.polich@gmail.com>
2022-10-30 22:34:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'modern-normalize/modern-normalize.css';
 import { App } from 'components/App';
+import { GlobalStyle } from './index.styled.js';
 import './index.styled.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
