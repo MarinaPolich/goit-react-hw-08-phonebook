52aaa81b071ef706679fdf6b625ed6489ea8beea
MarinaPolich <marina.polich@gmail.com>
2022-11-01 00:01:17 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,20 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = contact => {
     if (this.state.contacts.find(({ name }) => name === contact.name)) {
       alert(`${contact.name} is already in contacts.`);
