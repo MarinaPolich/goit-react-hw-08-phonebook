a4c7236e6c895045177cef9106ccc5f0877ecfcd
MarinaPolich <marina.polich@gmail.com>
2022-11-14 00:42:30 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,12 +1,18 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getAllContacts = state => state.contacts.items;
 
 export const getIsLoading = state => state.contacts.isLoading;
 
 export const getError = state => state.contacts.error;
 
-export const getFilterContacts = state =>
-  state.contacts.items.filter(({ name }) =>
-    name.toLowerCase().includes(state.filter.toLowerCase())
-  );
-
 export const getFilter = state => state.filter;
+
+export const getFilterContacts = createSelector(
+  [getAllContacts, getFilter],
+  (contacts, filter) => {
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
